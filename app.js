d12a00755104368b80822458d955d00551f69a6a
JenniferBautistaBarrios <230317@utxicotepec.edu.mx>
2025-01-25 23:53:48 -0600

    Practica 04
    Creación y corrección de los endpoints login, logout, update, status y listCurrentSessions

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 import express from 'express';
 import session from 'express-session';
 import requestIp from 'request-ip'
-import macaddress from 'macaddress'
+import moment from 'moment-timezone';
 import os from 'os';
 import { v4 as uuidv4 } from 'uuid';
-import { resolve } from 'path';
 
 const app = express();
 
-app.use(requestIp.mw());
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(session({
     secret: 'Practica04 JBB#Jenny-Sessiones',
     resave: false,
     saveUninitialized: true,
     cookie: {maxAge: 5*60*1000}
 }))
+
+const sessions = {};
+app.use(requestIp.mw());
+
 
 const getServerIp = () =>{
     const interfaces = os.networkInterfaces();
@@ -37,102 +40,153 @@ const getServerMac = () =>{
         }
     }
 }
-const getClientMac = () => {
-    return new Promise((resolve, reject) => {
-        macaddress.one((err, mac) => {
-            if (err) {
-                reject('Error al obtener la dirección MAC:', err);
-            }
-                resolve(mac);
-        });
-    })
-   
-}
 
-app.get('/login/:nombre/:email', async(req,res)=> {
-    if(!req.session.start){
-        req.session.start = new Date();
-        req.session.lastAccess = new Date();
-        req.session.uuid = uuidv4();
-        req.session.serverIp = getServerIp();
-        req.session.serverMac = getServerMac();
-        req.session.clientIp = req.socket.remoteAddress;
-        req.session.clientMac = await getClientMac();
-        req.session.name = req.params.nombre;
-        req.session.email = req.params.email;
-
-        res.send('Nueva sesion iniciada');
-    }else{
-        res.send('La sesión ya esta activa');
+setInterval(() => {
+    const now = Date.now();
+    for (const sessionId in sessions) {
+        const { lastAccess } = sessions[sessionId];
+        const sessionAge = now - new Date(lastAccess);
+        if (sessionAge > 5 * 60 * 1000) { // 5 minutos
+            delete sessions[sessionId];
+        }
     }
+}, 60 * 1000); 
+
+
+app.get('/',(req,res)=>{
+    return res.status(200).json({
+        message:"Bienvenido al API de Control de Sesiones",
+        author: "Jennifer Bautista Barrios"
+    })
+})
+
+app.post('/login', (req,res)=> {
+    const { name , email } = req.body;
+
+    if(!email || !name){
+        return res.status(400).json({error: "Datos no recibidos correctamente"})
+    }
+
+    const sessionId = uuidv4();
+
+    sessions[sessionId] = {
+        sessionId,
+        name,
+        email, 
+        start : moment(new Date()).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss'),
+        lastAccess : moment(new Date()).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss'),
+        lastActivity : moment(new Date()).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss'),
+        serverIp : getServerIp(),
+        serverMac : getServerMac(),
+        clientIp : req.socket.remoteAddress
+    }  
+
+    res.status(200).json({
+        message:"Se ha logeado de manera exitosa",
+        sessionId,
+    })
 })
 
 app.get('/update', (req,res) => {
-    if(!req.session.start){
-        req.session.lastAccess = new Date();
-        res.send('Datos de sesión actualizados.')
-    }else{
-        res.send('No hay una sesión activa.')
+    const {sessionId, name, email} = req.body
+    if(!sessionId || !name || !email){
+
+        return res.status(400).json({error: "Datos no recibidos correctamente"})
     }
+
+    if(email) sessions[sessionId].name = name;
+    if(email) sessions[sessionId].email = email;
+    const {lastAccess, lastActivity} = moment(new Date()).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
+    sessions[sessionId].lastAccess = lastAccess;
+    sessions[sessionId].lastActivity = lastActivity;
+
+    res.status(200).json({message:"Sus datos se han actualizado de manera exitosa"})
 })
 
-app.get('/listCurrentSessions', (req,res) => {
-    if (req.session.start){
-        const inicio = new Date(req.session.start);
-        const ultimoAccesso = new Date(req.session.lastAccess);
+app.get('/status', (req, res) => {
+    const sessionId = req.query.sessionId;
+
+    if (!sessionId || !sessions[sessionId]) {
+        return res.status(404).json({ message: "No existe una sesión activa" });
+    }
+
+    const start  = sessions[sessionId];
+    const inicio = new Date(start);
+    const ahora  = new Date();
+
+    const antiguedadMS = ahora - inicio;
+    const horas = Math.floor(antiguedadMS / (1000 * 60 * 60));
+    const minutos = Math.floor((antiguedadMS % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((antiguedadMS % (1000 * 60)) / 1000);
+
+
+    const lastActivity = moment(new Date()).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
+    sessions[sessionId].lastActivity = lastActivity;
+
+    res.status(200).json({
+        message: "Sesión Activa",
+        sessionTime: `${horas} horas, ${minutos} minutos, ${segundos} segundos`,
+        session: sessions[sessionId],
+    });
+});
+
+app.get('/listCurrentSessions', (req, res)=>{
+    const password = req.query.password;
+
+    if(password != 'Jennifer'){
+        return res.status(401).json({error: 'La contraseña es incorrecta'})
+    }
+
+    if(Object.keys(sessions).length === 0){
+        return res.status(200).json({message: "Actualmente no hay sesiones activas"})
+    }
+    const formatedSessions = Object.values(sessions).map(({ sessionId, name, email, start, lastAccess, lastActivity, serverIp, clientIp, serverMac }) => {
+
         const ahora = new Date();
+        const ultimaActividad = new Date(lastActivity);
+        const diferenciaMS = ahora - ultimaActividad;    
+        const minutos = Math.floor((diferenciaMS % (1000 * 60 * 60)) / (1000 * 60));
+        const segundos = Math.floor((diferenciaMS  % (1000 * 60)) / 1000);
 
-        // Calcular tiempo de la session
-        const antiguedadMS = ahora-inicio
-        const horas = Math.floor(antiguedadMS / (1000*60*60))
-        const minutos = Math.floor((antiguedadMS % (1000*60*60)) / (1000*60))
-        const segundos = Math.floor((antiguedadMS % (1000*60)) /1000)
-        
-        if(minutos >= 2){
-            req.session.destroy((error) => {
-                if(error){
-                    return res.status(500).send('Error al cerrar la sesión')
-                }
-                res.send('Sesión cerrada por tiempo de inactividad.')
-            })
+        return {
+            sessionId,
+            name,
+            email,
+            start,
+            lastAccess,
+            lastActivity,
+            inactivityTime : `${minutos}minutos ${segundos}segundos`,
+            serverIp,
+            serverMac,
+            clientIp
+        };
+    });
+
+    res.status(200).json({
+        message: "Bienvenido a su panel de administración de sesiones",
+        sessions: formatedSessions})
+
+});
+
+app.post('/logout', (req,res)=>{
+    const sessionId = req.query.sessionId;
+    if(!sessionId || sessions[sessionId]){
+        return res.status(404).json({message:"No se ha encontrado una sesión activa."})
+    }
+
+    delete sessions[sessionId];
+    req.session.destroy((error)=>{
+        if(error){
+            return res.status(500).json({error: 'Error al cerrar la sesión'})
         }
-    
-    
+    })
 
-        res.json ({
-            mensaje:'Estado de la sesión ',
-            nombreUsuario: req.session.name,
-            correo: req.session.email,
-            uuid: req.session.uuid,
-            ipServidor: req.session.serverIp,
-            macServidor: req.session.serverMac,
-            ipCliente: req.session.clientIp,
-            macCliente: req.session.clientMac,
-            inicio: inicio.toISOString(),
-            ultimoAcceso: ultimoAccesso.toISOString(),
-            sessionTime: `${horas} horas, ${minutos} minutos, ${segundos} segundos`
-
-        })
-    
-    }else{
-        res.send('No hay una sesión activa')
-    }
+    res.status(200).json({message: "Logout exitoso"})
 })
 
-app.get('/logout', (req,res)=>{
-    if(!req.session){
-        req.session.destroy((error) => {
-            if(error){
-                return res.status(500).send('Error al cerrar la sesión')
-            }
-            res.send('Sesión cerrada correctamente.')
-        })
-    }else{
-        res.send('No hay una sesión activa para cerrar.')
-    }
-})
 
-const PORT = 3000;
+
+const PORT = 3001;
 app.listen(PORT, ()=> {
     console.log(`Servidor escuchando en http://localhost:${PORT}`)
 })
